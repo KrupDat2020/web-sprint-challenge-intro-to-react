3f42c93132e861bbbc2b2becf25bc0818a86666f
KrupDat2020 <mark.krupinsky@gmail.com>
2021-04-16 11:07:38 -0500

    getting a module build failed error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Characters from './components/Characters'
 
 const App = () => {
+
+  const [Characters, setCharacters] = useState(Characters);
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/1');
+    .then(function(res) {
+      console.log(res.Characters)
+      setCharacters(res.Characters)
+    })
+    .catch(err) {
+      console.log(err);
+    }
+  }, []);
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,9 @@
 // Write your Character component here
+import React, { useState } from 'react';
+
+export default function Character (props) {
+
+    const {names} = props;
+
+    const []
+}
